2fe0fe1363849a12ead789a83d3a48ab9d3fcc97
Harshitha25 <harshiharsh254@gmail.com>
2023-06-02 15:43:00 +0530

    url and browser

--- Harshitha/src/test/java/practice1/PS2Test.java
+++ Harshitha/src/test/java/practice1/PS2Test.java
@@ -1,10 +1,25 @@
 package practice1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class PS2Test {
+	WebDriver driver;
 	@Test
 public void practice(){
+		String BROWSER = System.getProperty("browser");
+		String URL= System.getProperty("url");
+		 if(BROWSER.equalsIgnoreCase("chrome"))
+		 { WebDriverManager.chromedriver().setup();
+			 driver=new ChromeDriver();}
+		 if(BROWSER.equalsIgnoreCase("firefox"))
+		 { WebDriverManager.firefoxdriver().setup();
+			 driver=new FirefoxDriver();}
+		 
 	System.out.println("Hello");
 	System.out.println("Poll scm success");
 }
